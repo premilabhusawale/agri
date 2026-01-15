b56505b44f51d6d5d1f565079d71aacdae8c10ef
premilabhusawale <premilabhusawale@gmail.com>
2026-01-15 14:34:19 +0530

    changes

--- src/HeroSection/Hero.jsx
+++ src/HeroSection/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
       </div>
 
       {/* Content */}
-      <div className="container mx-auto px-4 relative z-10 py-15 bg-linear-to-tr from-slate-900/90 via-slate-900/70 to-transparent  p-8 lg:p-16">
+      <div className="w-full px-4 relative z-10 py-15 bg-linear-to-tr from-slate-900/90 via-slate-900/70 to-transparent  p-8 lg:p-16">
         <div className="max-w-3xl">
           <motion.div
             initial={{ opacity: 0, y: 30 }}
