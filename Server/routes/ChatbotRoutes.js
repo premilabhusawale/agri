d1b3ebb13d077cdc238adffcf3ccc4bb3cff6e7e
sakshiBhusawale <sakshibhusawale3@gmail.com>
2026-03-04 23:24:16 +0530

    Untrack node_modules and restore clean structure

--- Server/routes/ChatbotRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ChatbotController = require('../controllers/ChatbotController');
-
-router.post('/chatbot/ask', ChatbotController.askBot);
-router.get('/chatbot/history/:userId', ChatbotController.getChatHistory);
-
-module.exports = router;
--- Server/utils/createRoom.js
+++
@@ -1,5 +0,0 @@
-const createRoom = (user1, user2) => {
-  return [user1, user2].sort().join("_");
-};
-
-module.exports = createRoom;
